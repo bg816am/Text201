4f869a64fcba45d074353965e4c92e3809ca7a95
Brian Gould <brian.gould@gmail.com>
2020-08-18 17:55:53 -0400

    Lesson done

---
+++ Assets/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// creates a menu item in unity for a script of type state
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+ [TextArea (10,14)][SerializeField] private string storyText; //Use SerializeField to have it show up in Unity
+}
