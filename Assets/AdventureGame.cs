181b8dfdccdae4bead020764c1f1df3063184be4
Brian Gould <brian.gould@gmail.com>
2020-08-17 21:01:01 -0400

    Finished lesson

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -10,9 +10,9 @@ public class AdventureGame : MonoBehaviour
     void Start()
     {
         textComponent.text =
-            "You wake up in your room, nursing a headache. You remember the party, bits and pieces of" +
+            ("You wake up in your room, nursing a headache. You remember the party, bits and pieces of" +
             "it at least. You knew you couldn't make it home and you checked in here at the El Dorado Hotel" +
-            "Looking around the room you remember nothing past checking in";
+            "Looking around the room you remember nothing past checking in");
     }
 
     // Update is called once per frame
